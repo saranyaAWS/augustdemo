b8ac5b94c9e663e99f8621d4bd9fc5a83c33b1f8
Ubuntu <ubuntu@ip-172-31-0-46.eu-west-2.compute.internal>
2026-08-25 09:39:04 +0000

    webhook

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("welcome to jenkins cicd pipelines");
     }
 }
 
